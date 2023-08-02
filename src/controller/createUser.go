ac08c3d91ee3246341dd8d983dad03a45f042d5d
celso junior <celso_scjj@hotmail.com>
2023-08-01 23:37:34 -0300

    creating application routes 2

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,6 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"Documentos/github.com/CelsodvJunior/CRUD-GO/src/configuration/rest_err"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
 
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("You colled the rout worng!")
+	c.JSON(err.Code, err)
+}
