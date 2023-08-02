d17f1cbd60066122636c9df37082a778448cecc0
celso junior <celso_scjj@hotmail.com>
2023-08-02 00:05:50 -0300

    creating object of the user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,11 +2,21 @@ package controller
 
 import (
 	"Documentos/github.com/CelsodvJunior/CRUD-GO/src/configuration/rest_err"
+	"Documentos/github.com/CelsodvJunior/CRUD-GO/src/controller/model/request"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("You colled the rout worng!")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect filds, error=%$\n", err.Error))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
