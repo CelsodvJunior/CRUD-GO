e1a7c1ae094266251c2935d11505290218535c9e
celso junior <celso_scjj@hotmail.com>
2023-08-03 15:08:06 -0300

    validating request fields

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -4,17 +4,20 @@ import (
 	"Documentos/github.com/CelsodvJunior/CRUD-GO/src/configuration/rest_err"
 	"Documentos/github.com/CelsodvJunior/CRUD-GO/src/controller/model/request"
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	log.Println("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error))
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := rest_err.NewBadRequestError("Some fields are incorrect")
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
